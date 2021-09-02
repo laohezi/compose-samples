20a9fb0b6ee11f127875536b6c330993b9bbcd8a
hugomiao <Hugo@planetart.com>
2021-09-02 19:00:21 +0800

    add app1

--- JetNews/app1/src/main/java/com/example/app1/list/ListPage.kt
+++ JetNews/app1/src/main/java/com/example/app1/list/ListPage.kt
@@ -1,30 +1,30 @@
 package com.example.app1.list
 
 import SeriesPcuCategoryItem
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.example.app1.NavigationViewModel
 import com.example.app1.Screen
-import com.example.app1.appContext
 import com.example.app1.utils.getScreenWidth
 import com.example.app1.utils.px2dp
-import com.google.accompanist.coil.rememberCoilPainter
-import com.google.accompanist.glide.rememberGlidePainter
+import kotlin.math.log
 
 @Composable
 fun ListPage(
@@ -40,16 +40,11 @@ fun ListPage(
             LazyColumn(Modifier.fillMaxWidth()) {
                 viewModel.banner?.let {
                     item {
-                        Image(
-                            painter = rememberGlidePainter(request = it),
-                            contentDescription = "banner",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .aspectRatio(2f)
-                        )
+                       ImageWithLoading(url = it as String, imageViewModifier =
+                       Modifier.fillMaxWidth().aspectRatio(2.0f)
+                       )
                     }
                 }
-
                 items(viewModel.items) { item ->
                     InnerList(item = item)
                 }
@@ -64,7 +59,7 @@ fun ListPage(
 @Composable
 
 fun InnerList(item: SeriesPcuCategoryItem) {
-    Column {
+    Column(modifier = Modifier.padding(top = 12.dp)) {
         Text(text = item.title ?: "")
         if (item.childs.size > 0) {
             LazyRow() {
@@ -86,18 +81,19 @@ fun PcuItemAdapter(item: SeriesPcuCategoryItem, width: Dp, onClick: (() -> Unit)
     val navigationViewModel = viewModel(modelClass = NavigationViewModel::class.java)
     Column(Modifier.clickable(enabled = item.isSale()) {
         navigationViewModel.naviTo(Screen.DetailScreen)
-    }) {
-        Image(
-            painter = rememberGlidePainter(request = item.thumbnails[0]),
-            contentDescription = "Image",
-            modifier = Modifier
-                .width(width = width)
-                .aspectRatio(1f)
-        )
+    }.fillMaxWidth()) {
+
+       item.thumbnails.firstOrNull()?.let {
+           ImageWithLoading(url = it, imageViewModifier = Modifier
+               .fillMaxWidth()
+               .aspectRatio(1.0f) )
+       }
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(80.dp)
+                .height(80.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = item.title ?: "")
             Text(text = item.price?.toString() ?: "")
@@ -112,5 +108,31 @@ fun PcuItemAdapter(item: SeriesPcuCategoryItem, width: Dp, onClick: (() -> Unit)
 
 
 typealias  NT = (Screen) -> Unit
+
+@Composable
+fun ImageWithLoading(url:String, imageViewModifier: Modifier){
+    val painter = rememberImagePainter(url)
+    Box {
+        Image(
+            painter = painter,
+            contentDescription = "Image",
+            modifier =imageViewModifier,
+            contentScale = ContentScale.Crop
+        )
+        when(painter.state){
+            ImagePainter.State.Empty -> {
+
+            }
+            is ImagePainter.State.Loading ->{
+                CircularProgressIndicator(Modifier.align(Alignment.Center))
+            }
+            is ImagePainter.State.Error -> {
+                println("lalallacuole")
+            }
+        }
+
+    }
+
+}
 
 
